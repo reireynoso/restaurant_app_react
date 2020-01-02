27e18f132c9a48d009a1ac8d5e901ca1c94422d4
reireynoso <reireynoso@gmail.com>
2020-01-02 12:38:28 -0500

    started styling on Home Component

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -4,8 +4,14 @@ import React from 'react'
 const HomeComponent = () => {
    
     return (
-        <div>
-          hey
+        <div className="space-div">
+          <div style={{width: "100%", border: "solid black 5px", margin: "10%", borderRadius: "20px"}}>
+            <h1 style={{textAlign: "center", padding: "3% 0"}}>Welp (Restaurant App around Jersey City)</h1>
+
+            <div>
+              
+            </div>
+          </div>
         </div>
     )
 }
